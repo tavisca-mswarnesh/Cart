05e5eb688e27f485a5b7dc06e06006c2b87eca7c
tavisca-mswarnesh <mswarnesh@tavisca.in>
2019-08-06 23:18:47 +0530

    Added Category

---
+++ Cart/Cheese.cs
@@ -0,0 +1,11 @@
+﻿namespace CartApp
+{
+    public class Cheese : Product
+    {
+        public override string Name => "Cheese";
+
+        public override double Price => 10;
+
+        public override Category category => Category.Dairy;
+    }
+}
--- Cart/Mobile.cs
+++ Cart/Mobile.cs
@@ -4,5 +4,6 @@
     {
         public override string Name => "mobile";
         public override double Price =>100;
+        public override Category category => Category.Electronics;
     }
 }
