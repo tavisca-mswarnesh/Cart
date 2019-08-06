dd724ffcdcfd9bed965ead9d0a3a11af890bfdfd
tavisca-mswarnesh <mswarnesh@tavisca.in>
2019-08-06 12:02:48 +0530

    Added files in branch

---
+++ Cart/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CartApp
+{
+    public class CartItem
+    {
+        public Product Item { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+        
+    }
+}
---
+++ Cart/Laptop.cs
@@ -0,0 +1,8 @@
+﻿namespace CartApp
+{
+    public class Laptop : Product
+    {
+        public override string Name => "laptop";
+        public override double Price => 1000;
+    }
+}
